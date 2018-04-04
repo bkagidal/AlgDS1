067c2a9bb86a4ed32a7617543aad37ffbcff47af
Himavarsha Kagidala <himavarshakagidala@Himavarshas-MacBook-Pro.local>
2018-04-03 18:06:34 -0700

    heap App

--- src/main/java/com/lab/heap/App.java
+++ src/main/java/com/lab/heap/App.java
@@ -6,14 +6,20 @@ public static void main(String[] args) {
 		
 		Heap heap = new Heap(10);
 		
+		heap.insert(1);
+		heap.insert(23);
+		heap.insert(56);
+		heap.insert(3);
 		heap.insert(10);
-		heap.insert(100);
-		heap.insert(-10);
+		heap.insert(-5);
 		
 		heap.heapsort();
 		
 		System.out.println();
-		System.out.println(heap.getMax());
+	//	System.out.println(heap.getMax());
+		//System.out.println(heap.getMax());
+		//System.out.println(heap.getMax());
+		//System.out.println(heap.getMax());
 		
 	}
 
